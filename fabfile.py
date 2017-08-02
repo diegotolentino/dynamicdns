87862e41e07969f220739b9a0e273b2b3ce112a6
Diego Tolentino <diegotolentino@gmail.com>
2017-08-02 20:02:33 -0300

    Versão inicial

---
+++ fabfile.py
@@ -0,0 +1,66 @@
+# Encoding: UTF-8
+"""
+Usage:
+    fab [target] [action ...]
+
+fab --list
+fab -H your_ssh_host update_dns:domain='sub.mydomain.com',local_port=80,local_ip=111.222.121.212
+fab -H your_ssh_host update_dns:domain='sub.mydomain.com',local_port=80
+"""
+import urllib2
+from StringIO import StringIO
+
+from fabric.contrib import files
+from fabric.api import *
+from jinja2 import Template
+
+##############################################################################
+# Configurations
+##############################################################################
+env.use_ssh_config = True # Procura hosts no arquivo ~/.ssh/config
+
+##############################################################################
+# Tarefas executaveis
+##############################################################################
+@task
+def get_local_ip():
+    get_ip_url = 'http://ipinfo.io/ip'
+    try:
+        response = urllib2.urlopen(get_ip_url)
+    except:
+        raise Exception(u'Endereço "%s" não pode ser aberto' % get_ip_url)
+
+    assert response.getcode() == 200
+
+    local_ip = response.read().strip()
+    ip_token = local_ip.split('.')
+    assert len(ip_token) == 4
+
+    for toke in ip_token:
+        assert toke.isdigit()
+
+    return local_ip
+
+
+@task
+def update_dns(domain, local_port=80, local_ip=None, template='nginx.tpl'):
+    """ Atualiza o proxy do nginx """
+
+    if not local_ip:
+        local_ip = get_local_ip()
+
+    kwargs = {
+        'domain': domain,
+        'local_port': local_port,
+        'local_ip': local_ip
+    }
+
+    files.upload_template(
+        filename=template,
+        destination='/etc/nginx/sites-enabled/dynamicdns.' + domain,
+        context=kwargs,
+        use_jinja=True,
+        backup=False
+    )
+
+    run('/etc/init.d/nginx configtest && /etc/init.d/nginx reload')
---
+++ nginx.tpl
@@ -0,0 +1,19 @@
+# ----------------------------------------------------------------------------
+# !!CAUTION!!
+# This file is modified by dynamicdns, changes in this file may be lost in
+# future updates
+# ----------------------------------------------------------------------------
+server {
+    listen 127.0.0.1:80;
+    server_name {{domain}};
+
+    access_log /var/log/nginx/{{domain}}.access_log;
+    error_log /var/log/nginx/{{domain}}.error_log;
+
+    location / {
+        proxy_set_header X-Forwarded-For $proxy_add_x_forwarded_for;
+        proxy_set_header Host $http_host;
+        proxy_redirect off;
+        proxy_pass http://{{local_ip}}:{{local_port}};
+    }
+}
